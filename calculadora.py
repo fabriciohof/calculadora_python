af165e3dd0d33e6717cf54999805c21d2a3f6b9c
Fabrício Formentini <91196904+fabriciohof@users.noreply.github.com>
2022-10-18 00:29:27 -0300

    Add files via upload

---
+++ calculadora.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[14]:
+
+
+def soma():
+    return (x+y)
+    
+def subtracao():
+    return (x-y)
+    
+def multiplicacao():
+    return (x*y)
+    
+def divisao():
+    return (x/y)
+    
+menu =int(input("Qual operação deseja realizar? "'\n'"1:Soma"'\n'"2:Subtração"'\n'"3:Multiplicação"'\n'"4:Divisão"'\n'))
+while menu != 1 and menu != 2 and menu != 3 and menu != 4:
+    print("operação inválida")
+    menu = int(input('Digite novamente a operação que deseja: '))
+   
+   
+x = float(input("Digite o primeiro número: ")) 
+y = float(input("Digite o segundo número: "))
+
+
+if menu == 1:
+    print(f'Resultado: {x} + {y} =',soma())
+elif menu == 2:
+    print(f'Resultado: {x} - {y} =',subtracao())
+elif menu == 3:
+    print(f'Resultado: {x} x {y} =',multiplicacao())
+elif menu == 4:
+    print(f'Resultado: {x} ÷ {y} =',divisao())
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
